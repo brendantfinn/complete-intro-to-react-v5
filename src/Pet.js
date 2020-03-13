c41413aec880bd06defe08f64ada32c75e742c37
brendantfinn <brendantfinn@gmail.com>
2020-03-12 19:39:32 -0500

    jsx: installed babel & plugins

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export default function Pet ({ name, animal, breed }) {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
-};
+export default function Pet({ name, animal, breed }) {
+  // return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, name),
+  //     React.createElement("h2", {}, animal),
+  //     React.createElement("h2", {}, breed)
+  // ]);
+
+  // ALL JSX is transpiled into React.createElement like above
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h3>{breed}</h3>
+    </div>
+  );
+}
