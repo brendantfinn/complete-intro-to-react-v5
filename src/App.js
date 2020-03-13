4d66eeeb841050dd281661c64c09d54879b07ca4
brendantfinn <brendantfinn@gmail.com>
2020-03-12 20:11:24 -0500

    created searchparams component / now rendering that to app.js and removed the pet comonents from rendering

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // import ReactDOM from 'react-dom';
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   //   return React.createElement("div", {}, [
@@ -27,9 +27,10 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <SearchParams />
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
     </div>
   );
 };
